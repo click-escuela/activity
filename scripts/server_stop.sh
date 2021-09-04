e85d229a232107e85e755efc581969a1ca519f6a
Evelin Aragon <eve_aragon@hotmail.com>
2021-09-04 20:29:28 -0300

    fix logs

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+chmod +x /home/ec2-user/server/activity/logs
+chmod +x /home/ec2-user/server/activity/logs/error.log
+chmod +x /home/ec2-user/server/activity/logs/debug.log
 var="$(cat /home/ec2-user/server/activity/activity-service.pid)"
 sudo kill $var
 sudo rm -rf /home/ec2-user/server/activity/activity-service.pid
