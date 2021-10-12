b2622683d1edd46097edceef3b94c417fd0d2673
Evelin <eve_aragon@hotmail.com>
2021-10-12 17:41:26 -0300

    subir a produccion

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 chmod +x /home/ec2-user/server/activity
 chmod +x /home/ec2-user/server/activity/*.jar
-chmod +x /home/ec2-user/server/activity/server_start.sh
+chmod +x /home/ec2-user/server/activity/server_prestart.sh
 chmod +x /home/ec2-user/server/activity/server_stop.sh
 chmod +x /home/ec2-user/server/activity/server_clear.sh
 chmod +x /home/ec2-user/server/activity/fix_privileges.sh
--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-sudo rm -rf /home/ec2-user/server/activity
+sudo rm -rf /home/ec2-user/server/activity/appspec.yml
+sudo rm -rf /home/ec2-user/server/activity/activity-0.0.1-SNAPSHOT.jar
+sudo rm -rf /home/ec2-user/server/activity/activity-service.pid
+sudo rm -rf /home/ec2-user/server/activity/logs
+sudo rm -rf /home/ec2-user/server/activity/server_stop.sh
+sudo rm -rf /home/ec2-user/server/activity/fix_provileges.sh
+sudo rm -rf /home/ec2-user/server/activity/server_clear.sh
